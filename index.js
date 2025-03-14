4aa439f87d90a10bf4319b27c5b611fedfc7d2a7
alex-njugi <alex.karanja4@student.moringaschool.com>
2025-03-14 12:24:41 +0300

    done

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ newHeader.id = "victory";
 newHeader.textContent = "Alex Njugi Karanja is the champion";
 
 document.body.append(newHeader);
+
+// Step 1: Remove <main> element
+const main = document.getElementById("main");
+main.remove();
+
+// Step 2: Create a new <h1> element
+const newHeader = document.createElement("h1");
+
+// Step 3: Set the id of newHeader
+newHeader.id = "victory";
+
+// Step 4: Set text content
+newHeader.textContent = "Alex Njugi is the champion";
+
+// Step 5: Append newHeader to the DOM
+document.body.append(newHeader);
